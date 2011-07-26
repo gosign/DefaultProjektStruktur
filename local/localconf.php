dee68d39e50ee3caf77a90994c954511d83728ba
Lucas Jenß <lucas@x3ro.de>
2011-07-26 18:18:54 +0200

    Add directory for server-local configuration files

---
+++ local/localconf.php
@@ -0,0 +1,11 @@
+<?php
+require_once( PATH_site . 'typo3conf/localconf_global.php');
+
+## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
+
+$typo_db = 'database';
+$typo_db_username = 'user';
+$typo_db_password = 'password';
+$typo_db_host = 'localhost';
+
+?>
