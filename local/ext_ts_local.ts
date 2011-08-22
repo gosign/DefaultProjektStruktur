9e36206fa0ce957382470db5ae3d82d41215ca79
Lucas Jenss <lucas@x3ro.de>
2011-08-22 11:56:44 +0200

    Add example for baseURL setting

---
+++ local/ext_ts_local.ts
@@ -0,0 +1,5 @@
+# Use this file in order to specify Typoscript configuration
+# which is specific to the current environment, that is, the
+# current server the page is running on, such as the baseURL
+
+# config.baseURL = http://CHANGEME
