86a946b747219760afa44a56c432996e3bddc622
Caspar Stuebs <caspar@gosign.de>
2011-09-14 15:08:44 +0200

    add comments for sample debug config in local/localconf.php

--- local/localconf.php
+++ local/localconf.php
@@ -8,7 +8,11 @@ $typo_db_username = 'user';
 $typo_db_password = 'password';
 $typo_db_host = 'localhost';
 
+// If this config is set, TYPO3 writes sql errors to the FE
 //$TYPO3_CONF_VARS['SYS']['sqlDebug'] = '1';
+
+// This config enables the TYPO3 deprecation log.
+// It logs deprecated TYPO3 functions (not PHP functions!) to typo3conf/deprecation_{HASH}.log
 //$TYPO3_CONF_VARS['SYS']['enableDeprecationLog'] = '1';
 
 ?>
