90a11fc2ac8d8916c8a2c7fe82018cf4f1b1e043
Caspar Stuebs <caspar@gosign.de>
2011-09-22 16:22:22 +0200

    add local/realurl_conf.php according to the DefaultProjekt

---
+++ local/realurl_conf.php
@@ -0,0 +1,11 @@
+<?php
+require_once(PATH_site . 'typo3conf/realurl_conf_global.php');
+
+/*
+ * Use this configuration for your development server, according to typo3conf/realurl_conf_global.php
+ */
+// $TYPO3_CONF_VARS['EXTCONF']['realurl']['LOCAL_DOMAIN1'] = $TYPO3_CONF_VARS['EXTCONF']['realurl']['DOMAIN1'];
+
+// $TYPO3_CONF_VARS['EXTCONF']['realurl']['LOCAL_DOMAIN2'] = $TYPO3_CONF_VARS['EXTCONF']['realurl']['DOMAIN2'];
+
+?>
