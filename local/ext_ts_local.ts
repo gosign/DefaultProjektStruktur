c6b250755ab1b216ef50a13b8f1e3c0e365ebfd7
Caspar Stuebs <caspar@gosign.de>
2012-01-27 14:57:25 +0100

    ext_ts_local.ts add conditional baseURL as example

--- local/ext_ts_local.ts
+++ local/ext_ts_local.ts
@@ -2,7 +2,10 @@
 # which is specific to the current environment, that is, the
 # current server the page is running on, such as the baseURL
 
-# config.baseURL = http://CHANGEME
+# config.baseURL = http://CHANGEME/LIVEURL
+# [globalString = IENV:HTTP_HOST= *CHANGEME/DEVELOPMENT]
+# 	config.baseURL = http://CHANGEME/DEVELOPMENT
+# [global]
 
 # You may disable Queo-Speedup by default by setting the following option to "0"
 config.enableQueoSpeedup = 1
